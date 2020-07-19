8ee54c4bffdd63526b95c835c257ec8d6852d10d
Justin Mazzocchi <2831158+jzzocc@users.noreply.github.com>
2020-07-18 19:06:12 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Justin Mazzocchi on 7/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MetatextApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetatextApp.swift
+//  Shared
+//
+//  Created by Justin Mazzocchi on 7/18/20.
+//
+
+import SwiftUI
+
+@main
+struct MetatextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
