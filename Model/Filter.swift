14fc4cdef5e81b1391ac149ecff56bb18590b012
Justin Mazzocchi <2831158+jzzocc@users.noreply.github.com>
2020-08-29 23:49:32 -0700

    Mute linter warning

--- Model/Filter.swift
+++ Model/Filter.swift
@@ -33,7 +33,9 @@ extension Filter {
 }
 
 extension Array where Element == Filter {
+    // swiftlint:disable line_length
     // Adapted from https://github.com/tootsuite/mastodon/blob/bf477cee9f31036ebf3d164ddec1cebef5375513/app/javascript/mastodon/selectors/index.js#L43
+    // swiftlint:enable line_length
     func regularExpression() -> String? {
         guard !isEmpty else { return nil }
 
