943c0028dae1ac09e430115efc13ab72b17d0461
Justin Mazzocchi <2831158+jzzocc@users.noreply.github.com>
2021-01-09 17:38:00 -0800

    Update GRDB

--- DB/Package.swift
+++ DB/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["DB"])
     ],
     dependencies: [
-        .package(name: "GRDB", url: "https://github.com/metabolist/GRDB.swift.git", .revision("1becfa3")),
+        .package(name: "GRDB", url: "https://github.com/metabolist/GRDB.swift.git", .revision("bce80cb")),
         .package(path: "Mastodon"),
         .package(path: "Secrets")
     ],
