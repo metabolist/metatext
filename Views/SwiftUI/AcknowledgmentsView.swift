8bf5a757ea4d8d3a8a947cb2d63ac4eda91c6e9e
Justin Mazzocchi <2831158+jzzocc@users.noreply.github.com>
2021-02-22 16:02:23 -0800

    Update acknowledgements

--- Views/SwiftUI/AcknowledgmentsView.swift
+++ Views/SwiftUI/AcknowledgmentsView.swift
@@ -24,7 +24,7 @@ private extension AcknowledgmentsView {
     enum Acknowledgment: CaseIterable {
         case combineExpectations
         case grdb
-        case kingfisher
+        case sdwebimage
         case sqlcipher
     }
 }
@@ -36,8 +36,8 @@ extension AcknowledgmentsView.Acknowledgment {
             return "CombineExpectations"
         case .grdb:
             return "GRDB"
-        case .kingfisher:
-            return "Kingfisher"
+        case .sdwebimage:
+            return "SDWebImage"
         case .sqlcipher:
             return "SQLCipher"
         }
@@ -49,8 +49,8 @@ extension AcknowledgmentsView.Acknowledgment {
             return URL(string: "https://github.com/groue/CombineExpectations")!
         case .grdb:
             return URL(string: "https://github.com/groue/GRDB.swift")!
-        case .kingfisher:
-            return URL(string: "https://github.com/onevcat/Kingfisher")!
+        case .sdwebimage:
+            return URL(string: "https://github.com/SDWebImage/SDWebImage")!
         case .sqlcipher:
             return URL(string: "https://www.zetetic.net/sqlcipher")!
         }
@@ -92,18 +92,16 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-        case .kingfisher:
+        case .sdwebimage:
             return """
-The MIT License (MIT)
-
-Copyright (c) 2019 Wei Wang
+Copyright (c) 2009-2020 Olivier Poitrey rs@dailymotion.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
@@ -113,8 +111,8 @@ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 """
         case .sqlcipher:
             return """
