822fa06046e2d84bb88a8235738ff83a3fa00d24
Justin Mazzocchi <2831158+jzzocc@users.noreply.github.com>
2022-11-21 20:46:18 -0800

    Formatting

--- View Controllers/SwipeableNavigationController.swift
+++ View Controllers/SwipeableNavigationController.swift
@@ -5,21 +5,19 @@ import UIKit
 // ref: https://stackoverflow.com/a/60598558/3797903
 class SwipeableNavigationController: UINavigationController {
     private lazy var fullWidthBackGestureRecognizer = UIPanGestureRecognizer()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupFullWidthBackGesture()
     }
-    
+
     private func setupFullWidthBackGesture() {
-        guard
-            let targets = interactivePopGestureRecognizer?.value(forKey: "targets")
-        else { return }
-        
+        guard let targets = interactivePopGestureRecognizer?.value(forKey: "targets") else { return }
+
         // have fullWidthBackGestureRecognizer execute the same handler as interactivePopGestureRecognizer
         fullWidthBackGestureRecognizer.setValue(targets, forKey: "targets")
         fullWidthBackGestureRecognizer.delegate = self
-        
+
         view.addGestureRecognizer(fullWidthBackGestureRecognizer)
     }
 }
